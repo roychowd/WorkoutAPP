1ed5c6f116ab590afe4e26ecf6c5238a43202f14
Ankush Sharma <ankush1@ualberta.ca>
2018-08-05 11:39:04 -0600

    conversion

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Navbar from '../Navbar/Navbar';
 import './Homepage.css';
 import GitLogo from './GitLogo.png';
@@ -9,61 +9,64 @@ import Runner from './runner.png';
 import $ from 'jquery';
 
 var loggedIn=false;
-const Homepage = () => {
-  return(
-    <div className='topdiv'>
-      <Navbar loggedIn={loggedIn} className='navbar'/>
-      {/* //==========HERE IS THE FIRST SECTION===========// */}
-      <div className='col1'>
-        {/* //===========HERE IS WHERE THE SIGN IN FORM IS DISPLAYED=======// */}
-        <main className="pa4 80 FormFontColor">
-          <form className="measure center">
-            <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
-              <legend className="f4 fw8 ph0 mh0">Sign In</legend>
-              <div className="mt3">
-                <label className="db fw8 lh-copy f6 fonts" for="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent bg-black hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+class Homepage extends Component{
+  render() {
+    return(
+      <div className='topdiv'>
+        <Navbar loggedIn={loggedIn} className='navbar'/>
+        {/* //==========HERE IS THE FIRST SECTION===========// */}
+        <div className='col1'>
+          {/* //===========HERE IS WHERE THE SIGN IN FORM IS DISPLAYED=======// */}
+          <main className="pa4 80 FormFontColor">
+            <form className="measure center">
+              <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+                <legend className="f4 fw8 ph0 mh0">Sign In</legend>
+                <div className="mt3">
+                  <label className="db fw8 lh-copy f6 fonts" for="email-address">Email</label>
+                  <input className="pa2 input-reset ba bg-transparent bg-black hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                </div>
+                <div className="mv3">
+                  <label className="db fw8 lh-copy f6 fonts" for="password">Password</label>
+                  <input className="b pa2 input-reset ba bg-transparent bg-black hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+                </div>
+                <label className="pa0 ma0 lh-copy f6 fw8 pointer"><input type="checkbox"/> Remember me </label>
+              </fieldset>
+              <div className="">
+                {/* //Change this to a button pls */}
+                <button href='/Dashboard' className="b ph3 pv2 input-reset ba b--black  bg-black grow pointer f6 dib datButton" type='submit'>Sign In </button>
               </div>
-              <div className="mv3">
-                <label className="db fw8 lh-copy f6 fonts" for="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent bg-black hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+              <div className="lh-copy mt3 FormFontColor">
+                {/* <a href="/Signup" class="f6 fw8 link  dim FormFontColor db">Sign up</a> */}
+                {/* <a href="#0" class="f6  fw8 link dim FormFontColor db">Forgot your password?</a> */}
               </div>
-              <label className="pa0 ma0 lh-copy f6 fw8 pointer"><input type="checkbox"/> Remember me </label>
-            </fieldset>
-            <div className="">
-              {/* //Change this to a button pls */}
-              <button href='/Dashboard' className="b ph3 pv2 input-reset ba b--black  bg-black grow pointer f6 dib datButton" type='submit'>Sign In </button>
-            </div>
-            <div className="lh-copy mt3 FormFontColor">
-              {/* <a href="/Signup" class="f6 fw8 link  dim FormFontColor db">Sign up</a> */}
-              {/* <a href="#0" class="f6  fw8 link dim FormFontColor db">Forgot your password?</a> */}
-            </div>
-          </form>
-        </main>
-      </div>
-      {/* //======SECOND BANNER========// */}
-      <div className='col2'>
-        <h2 className='peptalk'>Being able to track your Foods and Excercise is the key to achieving</h2>
-        <h3 className='peptalk' style={{color:'white'}}>Your Fitness Goals</h3>
-        <h5 className='peptalk'>We wish to bring the message of a healthy lifestyle to the students of the University of Alberta, and provide them with the neccessary tools to do so!
-        </h5>
-        {/* <div id='homepageCircle' className='"row'>
-          <div className='coolCircle col-sm-6'>30</div>
-          <div className='coolCircle col-sm-6'>30</div>
-        </div> */}
-        <circle className='circle'><img className='icon' src={Dumbell}/></circle>
-        <circle className='circle'><img className='icon' src={Runner}/></circle>
-        <circle className='circle'><img className='icon' src={Burger}/></circle>
+            </form>
+          </main>
+        </div>
+        {/* //======SECOND BANNER========// */}
+        <div className='col2'>
+          <h2 className='peptalk'>Being able to track your Foods and Excercise is the key to achieving</h2>
+          <h3 className='peptalk' style={{color:'white'}}>Your Fitness Goals</h3>
+          <h5 className='peptalk'>We wish to bring the message of a healthy lifestyle to the students of the University of Alberta, and provide them with the neccessary tools to do so!
+          </h5>
+          {/* <div id='homepageCircle' className='"row'>
+            <div className='coolCircle col-sm-6'>30</div>
+            <div className='coolCircle col-sm-6'>30</div>
+          </div> */}
+          <circle className='circle'><img className='icon' src={Dumbell}/></circle>
+          <circle className='circle'><img className='icon' src={Runner}/></circle>
+          <circle className='circle'><img className='icon' src={Burger}/></circle>
 
+        </div>
+        {/* //==============Third SECTION==========// */}
+        <div className='col3'>
+          <h5 className='bottomhead'>Created by <a href='https://github.com/AnkushSharma2698'>Ankush Sharma</a> and <a href='https://github.com/roychowd'>Dhruba Chowdhury</a> </h5>
+          <a className='cover' href='https://github.com/roychowd/WorkoutAPP'><img className='gitLogo pointer' src={GitLogo}/></a>
+        </div>
       </div>
-      {/* //==============Third SECTION==========// */}
-      <div className='col3'>
-        <h5 className='bottomhead'>Created by <a href='https://github.com/AnkushSharma2698'>Ankush Sharma</a> and <a href='https://github.com/roychowd'>Dhruba Chowdhury</a> </h5>
-        <a className='cover' href='https://github.com/roychowd/WorkoutAPP'><img className='gitLogo pointer' src={GitLogo}/></a>
-      </div>
-    </div>
-  )
+    )
+  }
 }
+
 
 export default Homepage;
 
